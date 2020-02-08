ffbaeca89eb9af8f24928b671361422fc04d4960
Joseph E Auz <joseph.auz77@gmail.com>
2020-02-08 10:48:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,11 +23,11 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  while hash[key] do
+  if hash[key] 
     hash[key] += 1
-  end 
-    hash[key] = 1
-  
+    else
+      hash[key] = 1
+  end
 end
 
 
